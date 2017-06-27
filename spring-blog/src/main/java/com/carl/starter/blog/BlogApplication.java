269f6a1a80a94610a5ed6b3be04938d31041f311
Carl <75688443@qq.com>
2017-06-27 18:37:32 +0800

    添加分布式的服务

--- spring-blog/src/main/java/com/carl/starter/blog/BlogApplication.java
+++ spring-blog/src/main/java/com/carl/starter/blog/BlogApplication.java
@@ -2,8 +2,11 @@ package com.carl.starter.blog;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @SpringBootApplication
-@RestController
 @RefreshScope
+@ComponentScan
+@EnableAutoConfiguration
+@EnableEurekaClient
+@RestController
 public class BlogApplication {
     @Value("${blog_name:carl!}")
     private String name;
