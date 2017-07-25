9e879f46b75805ae66817f4a47ed48979776168b
huangwb <huangwb@sunrizetech.com>
2017-07-25 09:05:56 +0800

    test stash

--- spring-auth-center/src/main/java/com/carl/starter/auth/AuthServerApplication.java
+++ spring-auth-center/src/main/java/com/carl/starter/auth/AuthServerApplication.java
@@ -71,7 +71,7 @@ public class AuthServerApplication extends WebMvcConfigurerAdapter {
             });
             http.formLogin().loginPage("/login").permitAll().and().authorizeRequests()
                     .anyRequest().authenticated();
-            //test stash
+            //test stash test stash2
         }
 
         @Override
